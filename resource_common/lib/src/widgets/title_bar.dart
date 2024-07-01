2f7147e8774cb74004cb5e7bf704c5ed5720ca18
LI YUAN XIN <yuanxintaxon@gmail.com>
2024-07-01 20:54:54 +0800

    remove unused imports

--- resource_common/lib/src/widgets/title_bar.dart
+++ resource_common/lib/src/widgets/title_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:resource_common/resource_common.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class TitleBar extends StatelessWidget implements PreferredSizeWidget {
   const TitleBar({
