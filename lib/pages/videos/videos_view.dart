98a8fb7da46d2193f7a1d445c1a09868420ffaca
LI YUAN XIN <yuanxintaxon@gmail.com>
2024-07-11 21:01:11 +0800

    bugfix flickering

--- lib/pages/videos/videos_view.dart
+++ lib/pages/videos/videos_view.dart
@@ -15,64 +15,66 @@ class VideosPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Styles.c_2E2E2E,
-      body: Obx(
-        () => Stack(
-          alignment: Alignment.topLeft,
-          children: [
-            if (logic.data.isNotEmpty) ...[
-              ShortVideoScroller(
-                // Called every time video page is changed.
-                updateLastSeenPage: (lastSeenPageIndex) {
-                  // logic.updateBrowserUrl(lastSeenPageIndex);
-                },
-                onPageChanged: (pageIndex) {
-                  // logic.disableAutoPlay();
-                  logic.updateBrowserUrl(pageIndex);
-                },
-                onPlaying: () {
-                  logic.enableAutoPlay();
-                },
-                onBack: () {
-                  logic.returnHome();
-                },
-                // Video data.
-                listData: logic.data,
-                autoPlay: logic.autoPlay.value,
-                // Watched videos, it's updated every time new video is watched.
-                videoWatched: logic.videoWatched,
-                pageSize: 4,
-                enableBackgroundContent: false,
-                // Load more video data.
-                loadMore: (page, pageSize) async {
-                  // Just for test.
-                  debugPrint("load more...");
-                  debugPrint("Video ${logic.videoWatched}");
-                  // List<Map<String, dynamic>> newData =
-                  //     await service.mapData((page + 2), 4);
-                  // if (newData.isNotEmpty) {
-                  //   setState(() {
-                  //     data = [...data, ...newData];
-                  //   });
-                  // }
-                  //.
-                },
-                // Your custom widget.
-                // likeWidget: (video, updateData) {
-                //   return LikeWidget(
-                //     likes: video?.likes ?? 0,
-                //     liked: video?.liked ?? false,
-                //     updateData: updateData,
-                //     id: video?.id,
-                //   );
-                // },
-                shareWidget: _shareWidget,
-              ),
-              if (logic.showShareModal.value &&
-                  logic.videoToShare.value != null)
-                _buildShareModal(),
-            ],
-          ],
-        ),
+      body: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Obx(
+            () => logic.data.isNotEmpty
+                ? ShortVideoScroller(
+                    // Called every time video page is changed.
+                    updateLastSeenPage: (lastSeenPageIndex) {
+                      // logic.updateBrowserUrl(lastSeenPageIndex);
+                    },
+                    onPageChanged: (pageIndex) {
+                      // logic.disableAutoPlay();
+                      logic.updateBrowserUrl(pageIndex);
+                    },
+                    onPlaying: () {
+                      logic.enableAutoPlay();
+                    },
+                    onBack: () {
+                      logic.returnHome();
+                    },
+                    // Video data.
+                    listData: logic.data,
+                    autoPlay: logic.autoPlay.value,
+                    // Watched videos, it's updated every time new video is watched.
+                    videoWatched: logic.videoWatched,
+                    pageSize: 4,
+                    enableBackgroundContent: false,
+                    // Load more video data.
+                    loadMore: (page, pageSize) async {
+                      // Just for test.
+                      debugPrint("load more...");
+                      debugPrint("Video ${logic.videoWatched}");
+                      // List<Map<String, dynamic>> newData =
+                      //     await service.mapData((page + 2), 4);
+                      // if (newData.isNotEmpty) {
+                      //   setState(() {
+                      //     data = [...data, ...newData];
+                      //   });
+                      // }
+                      //.
+                    },
+                    // Your custom widget.
+                    // likeWidget: (video, updateData) {
+                    //   return LikeWidget(
+                    //     likes: video?.likes ?? 0,
+                    //     liked: video?.liked ?? false,
+                    //     updateData: updateData,
+                    //     id: video?.id,
+                    //   );
+                    // },
+                    shareWidget: _shareWidget,
+                  )
+                : Container(),
+          ),
+          Obx(
+            () => logic.showShareModal.value && logic.videoToShare.value != null
+                ? _buildShareModal()
+                : Container(),
+          ),
+        ],
       ),
     );
   }
